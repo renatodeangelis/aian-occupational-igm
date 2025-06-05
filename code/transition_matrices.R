b4194756ae5e14511399c5b84e33003608c85ef9
Renato de Angelis <renatodeangelis12@gmail.com>
2025-06-05 16:02:32 -0500

    plausible graph

--- code/transition_matrices.R
+++ code/transition_matrices.R
@@ -2,6 +2,8 @@ library(dplyr)
 library(readr)
 library(tidyr)
 library(tibble)
+library(ggplot2)
+library(patchwork)
 
 macro_order = c("farming", "blue_col", "white_col", "unemp")
 meso_order = c("farming", "unskilled", "crafts", "clerical", "prof", "unemp")
@@ -14,18 +16,18 @@ data = read_csv("aian_weighted.csv") |>
 
 pi_0 = function(data, level) {
   df = data |> 
-    group_by(across(all_of(level))) |>
+    group_by({{ level }}) |>
     summarise(total_w = sum(weight),
                          .groups = "drop") |>
     mutate(pi0 = total_w / sum(total_w)) |>
-    arrange(across(all_of(level)))
+    arrange({{ level }})
   
   pi0_vec = df$pi0
-  names(pi0_vec) = df[[level]]
+  names(pi0_vec) = df |> pull({{ level }})
   return(pi0_vec)
 }
 
-p_matrix = function(data, level_dad, level_son) {
+p_matrix = function(data, level_dad, level_son, matrix = TRUE) {
   transition_df = data |>
     group_by({{ level_dad }}, {{ level_son }}) |>
     summarise(
@@ -47,7 +49,7 @@ p_matrix = function(data, level_dad, level_son) {
     ) |>
     column_to_rownames(var = rlang::as_string(rlang::enexpr(level_dad)))
   
-  as.matrix(p_mat)
+  ifelse(matrix == TRUE, return(transition_df), return(p_mat))
 }
 
 pi_star = function(p_mat) {
@@ -60,6 +62,74 @@ pi_star = function(p_mat) {
   return(pi_star)
 }
 
+p_mat = p_matrix(data, dad_meso, occ_meso)
+g = ggplot(
+  p_mat |> 
+    mutate(dad_meso = factor(dad_meso, levels = rev(unique(dad_meso)))),
+  aes(x = occ_meso, y = dad_meso, fill = P)) +
+  geom_tile(color = "white") +
+  geom_text(aes(label = sprintf("%.2f", P)),
+            size = 3, color = "black") +
+  scale_fill_gradient(low = "white", high = "steelblue") +
+  labs(x = "Son occupation",
+       y = "Father occupation",
+       fill = "Transition Prob.",
+       title = "P") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.text.y = element_text(angle = 45, hjust = 1),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title = element_text(size = 10),
+        legend.position = "bottom",
+        plot.title = element_text(hjust = 0.5))
+
+pi0_vec = pi_0(data, dad_meso)
+df_pi0 = tibble(
+  father = names(pi0_vec),
+  pi0 = as.numeric(pi0_vec)) |>
+  mutate(father = factor(father, levels = rev(unique(father))))
+
+g0 = ggplot(df_pi0, aes(x = 1, y = father, fill = pi0)) +
+  geom_tile(color = "white") +
+  geom_text(aes(label = sprintf("%.2f", pi0)),
+            size = 3, color = "black") +
+  scale_fill_gradient(low = "white", high = "steelblue") +
+  labs(title = "π_0") +
+  theme_minimal() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_text(angle = 45, hjust = 1),
+        axis.ticks.y = element_blank(),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5))
+
+
+steady = pi_star(p_matrix(data, dad_meso, occ_meso, FALSE))
+df_pi_star = tibble(
+  father = names(steady),
+  pi_star = as.numeric(steady)) |>
+  mutate(father = factor(father, levels = rev(unique(father))))
+
+g_star = ggplot(df_pi_star, aes(x = 1, y = father, fill = pi_star)) +
+  geom_tile(color = "white") +
+  geom_text(aes(label = sprintf("%.2f", pi_star)),
+            size = 3, color = "black") +
+  scale_fill_gradient(low = "white", high = "steelblue") +
+  labs(title = "π*") +
+  theme_minimal() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_text(angle = 45, hjust = 1),
+        axis.ticks.y = element_blank(),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5))
+
+combined_plot = g + g0 + g_star + plot_layout(widths = c(6, 1, 1))
 
 
 
